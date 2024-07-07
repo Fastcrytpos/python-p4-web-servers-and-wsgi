9b5ecc839c1d4cc0831c9167f0e1d90e6f322f0e
Samson Mubeya <rasspartah@gmail.com>
2024-07-07 22:11:33 +0300

    done

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request,Response
+from flask import Flask
+
+# app= Flask(__name__)
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response("AWSGI generated this response!")
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
+
